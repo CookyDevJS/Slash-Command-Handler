1dc2b679313f49917cf815e4411d9d98ed23d43c
Cooky <escobamatos@gmail.com>
2023-05-16 15:07:52 +0200

    Update slash.js

--- slash.js
+++ slash.js
@@ -2,6 +2,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const { readdirSync } = require('fs');
 const path = require('path');
+const config = require("./config.json")
 require('colors');
 require('dotenv').config();
 
@@ -13,14 +14,14 @@ readdirSync("./slash/").map(async dir => {
 	commands.push(require(path.join(__dirname, `./slash/${dir}/${cmd}`)))
     })
 })
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(config.token);
 
 (async () => {
 	try {
 		console.log('[Discord API] Started refreshing application (/) commands.'.yellow);
 		await rest.put(
 			// si quieres hacer tus comandos de barra en todos los servidores usa "applicationCommands("CLIENT_ID")"
-			Routes.applicationGuildCommands(process.env.BOTID, process.env.SERVERID),
+			Routes.applicationGuildCommands(config.botID, config.serverID),
 			{ body: commands },
 		);
 		console.log('[Discord API] Successfully reloaded application (/) commands.'.green);
